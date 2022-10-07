947563bd2eb0daafbab285defed5dbc6d700ffd4
Sourabh Mittal <sourabhm465@gmail.com>
2022-10-07 21:46:22 +0530

    AudioEngine
    Sound Example 1 - One spaceship bounces off the d\edges of the window. A different sound effect is played for each edge Demonstrates using the XACT audio engine to play game sounds.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sound
+
+Sound Example 1 - One spaceship bounces off the d\edges of the window. A different sound effect is played for each edge Demonstrates using the XACT audio engine to play game sounds.
  
---
+++ SoundExample1/spacewar.h
@@ -0,0 +1,42 @@
+
+#ifndef _SPACEWAR_H             // Prevent multiple definitions if this 
+#define _SPACEWAR_H             // file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include "game.h"
+#include "textureManager.h"
+#include "image.h"
+#include "planet.h"
+#include "ship.h"
+
+//=============================================================================
+// This class is the core of the game
+//=============================================================================
+class Spacewar : public Game
+{
+private:
+    // game items
+    TextureManager nebulaTexture;   // nebula texture
+    TextureManager gameTextures;    // game texture
+    Ship    ship;           		// spaceship
+    Planet  planet;                 // the planet
+    Image   nebula;                 // backdrop image
+
+public:
+    // Constructor
+    Spacewar();
+
+    // Destructor
+    virtual ~Spacewar();
+
+    // Initialize the game
+    void initialize(HWND hwnd);
+    void update();      // must override pure virtual from Game
+    void ai();          // "
+    void collisions();  // "
+    void render();      // "
+    void releaseAll();
+    void resetAll();
+};
+
+#endif
